1093e0210d263346c5f22b2a4aaa2489a2993e6b
Ruairidh MacLeod <ruairidh@rkm.dev>
2020-07-30 10:17:24 +0000

    add flake8-bugbear and flake8-assert-msg

--- tests/flake8_assert_msg_test.py
+++ tests/flake8_assert_msg_test.py
@@ -9,13 +9,15 @@ def results(s: str) -> Set[str]:
 
 
 def test_trivial() -> None:
-    assert not results("")
+    assert not results(""), "trivial input should be ok"
 
 
 def test_normal_assignment_ok() -> None:
-    assert not results('assert True, "foo"')
+    assert not results('assert True, "foo"'), "assertion with msg should be ok"
 
 
 def test_assignment_expression_not_ok() -> None:
     (msg,) = results("assert True")
-    assert msg == "1:0: ASS001 do not use bare asserts"
+    assert (
+        msg == "1:0: ASS001 do not use bare asserts"
+    ), "bare assert should raise a warning"
